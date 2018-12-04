df3848ffe92f67342bd903ea6cc782a3113a110e
Shashank Prasanna <shashank4@gmail.com>
2018-12-03 17:13:49 -0800

    fixed container entry script issue

--- cifar10_train.py
+++ cifar10_train.py
@@ -115,7 +115,7 @@ def main():
     summary = train(model, lr_schedule, opt, train_set_aug, test_set, 
           batch_size=batch_size, loggers=(TableLogger(), TSV), timer=t, test_time_in_total=False, drop_last=True)
         
-    with open('/datasets/logs_job_id_'+str(job_id)+'.csv', 'w') as csvfile:
+    with open('/datasets/results_job_id_'+str(job_id)+'.log', 'w') as csvfile:
         cw = csv.writer(csvfile, delimiter=',')
         for key, val in summary.items():
             cw.writerow([key, val])    
