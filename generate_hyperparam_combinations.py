c9c6f98fde3ab2128d8ffe7df068c17643512743
Shashank Prasanna <shashank4@gmail.com>
2018-12-17 11:36:02 -0800

    added better output messages

--- generate_hyperparam_combinations.py
+++ generate_hyperparam_combinations.py
@@ -1,14 +1,6 @@
 import itertools
 import yaml
 
-#hyper_params = {
-#    'max_learning_rate': [0.2, 0.4, 0.6],
-#    'data_aug_cutout_size': [5,8,12],
-#    'batch_size': [32,128,512],
-#    'momentum': [0.9, 0.95, 0.99],
-#    'batch_norm': ['on', 'off']
-#}
-
 hyper_params = {
     'max_learning_rate': [0.4, 0.6],
     'data_aug_cutout_size': [5, 12],
@@ -19,7 +11,10 @@ hyper_params = {
 
 keys, values = zip(*hyper_params.items())
 hyperparam_allsets = [dict(hyperparam_set=dict(zip(keys, v))) for v in itertools.product(*values)]
-print("Total number of hyperparameter sets: ",len(hyperparam_allsets))
+print("Total number of hyperparameter sets: "+str(len(hyperparam_allsets)))
 
 with open('hyperparams.yml', 'w') as outfile:
     yaml.dump(hyperparam_allsets, outfile, default_flow_style=False)
+
+print("Hyperparameter sets saved to: "+'hyperparams.yml')
+
